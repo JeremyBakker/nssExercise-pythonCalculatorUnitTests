7c962e75b6608b248ee8d12efda1741a33b7533f
Jeremy Bakker <jeremybakker@Jeremys-MacBook-Pro.local>
2017-04-11 08:52:28 -0500

    completed exercise

---
+++ calculator.py
@@ -0,0 +1,44 @@
+class Calculator():
+    '''Performs four mathematical operations
+    Methods:
+     add(number, number)
+     subtract(number, number)
+     multiply(number, number)
+     divide(number,number)
+    '''
+
+    def add(self, number1, number2):
+        '''Add two numbers 
+        Arguments:
+          number1 = any number
+          number2 = any number
+        '''
+
+        return number1 + number2
+
+    def subtract(self, number1, number2):
+        '''Subtract number2 from number1
+        Arguments:
+          number1 = any number
+          number2 = any number
+        '''
+
+        return number1 - number2
+
+    def multiply(self, number1, number2):
+        '''Multiply two numbers
+        Arguments:
+          number1 = any number
+          number2 = any number
+        '''
+
+        return number1 * number2
+
+    def divide(self, number1, number2):
+        '''Divide number1 by number2
+        Arguments:
+          number1 = any number
+          number2 = any number
+        '''
+
+        return number1 / number2
